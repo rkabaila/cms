0fbc1a443cb0b19b4fb9f47f08ab873e916484e4
Rimas Kabaila <rimas.kabaila@gmail.com>
2016-03-09 13:31:06 +0200

    login + security

---
+++ src/Custom/CMSBundle/Controller/SecurityController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rimas
+ * Date: 3/9/16
+ * Time: 12:50 PM
+ */
+
+
+namespace Custom\CMSBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+class SecurityController extends Controller
+{
+    public function loginAction(Request $request)
+    {
+        $session = $request->getSession();
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContextInterface::AUTHENTICATION_ERROR
+            );
+        } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+        return $this->render(
+            'CustomCMSBundle:Security:login.html.twig',
+            array(
+                'error'         => $error,
+            )
+        );
+    }
+}
